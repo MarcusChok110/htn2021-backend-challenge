e01db1aec3bcf13dfc0472834cde251a0c7ff12b
Marcus Chok <marcus.chok110@gmail.com>
2021-03-10 12:23:28 -0500

    Fix type of values to attributes and add function to map over skill array

--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -1,4 +1,4 @@
-import Skill, { SkillInstance } from '../models/skillModel';
+import Skill, { SkillAttributes, SkillInstance } from '../models/skillModel';
 
 const skillService = {
   async findAll() {
@@ -7,10 +7,10 @@ const skillService = {
   async findAllFromUser(user_id: number) {
     return await Skill.findAll({ where: { user_id } });
   },
-  async update(id: number, values: SkillInstance) {
+  async update(id: number, values: SkillAttributes) {
     return await Skill.update(values, { where: { id } });
   },
-  async updateOrCreate(user_id: number, values: SkillInstance) {
+  async updateOrCreate(user_id: number, values: SkillAttributes) {
     const skill = await Skill.findOne({
       where: { user_id, name: values?.name },
     });
@@ -18,6 +18,9 @@ const skillService = {
       return await Skill.create({ ...values, user_id });
     }
     return await Skill.update(values, { where: { id: skill.id } });
+  },
+  extractSkills(skills: SkillInstance[]) {
+    return skills.map((skill) => skill.get());
   },
 };
 
