9746b902c1932bc13a5fd3649c5d8d6e3eb409b8
Marcus Chok <marcus.chok110@gmail.com>
2021-03-09 22:53:24 -0500

    Add basic middleware to server

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import express from 'express';
 import db from './db/index';
+import router from './routes/index';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(router);
+
 app.listen(PORT, async () => {
   try {
     await db.authenticate();
     await db.sync();
-    console.log('success');
+    console.log(`Successfully listening on port ${PORT}`);
   } catch (error) {
-    console.error('failure', error);
+    console.error('Failed to start server / connect to database:', error);
   }
 });
