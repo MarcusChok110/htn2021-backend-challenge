0c27b24cdb96e2807de75a8e7ec19dfc03a9a1a8
Marcus Chok <marcus.chok110@gmail.com>
2021-03-10 14:25:03 -0500

    Add functions for creating and deleting skills for users and add documentation

--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -7,10 +7,17 @@ const skillService = {
   async findAllFromUser(user_id: number) {
     return await Skill.findAll({ where: { user_id } });
   },
+  /**
+   * Update a skill which already exists in the database
+   */
   async update(id: number, values: SkillAttributes) {
     return await Skill.update(values, { where: { id } });
   },
-  async updateOrCreate(user_id: number, values: SkillAttributes) {
+  /**
+   * Creates a skill for the given user_id if it does not exist or updates it if it
+   * does exist
+   */
+  async save(user_id: number, values: SkillAttributes) {
     const skill = await Skill.findOne({
       where: { user_id, name: values?.name },
     });
@@ -21,6 +28,18 @@ const skillService = {
   },
   extractSkills(skills: SkillInstance[]) {
     return skills.map((skill) => skill.get());
+  },
+  /**
+   * Insert a skill which doesn't already exist in the database
+   */
+  async create(user_id: number, values: SkillAttributes) {
+    return await Skill.create({ ...values, user_id });
+  },
+  /**
+   * Deletes all skills with user_id given
+   */
+  async deleteByUserId(user_id: number) {
+    return await Skill.destroy({ where: { user_id } });
   },
 };
 
